398a0fd144f7504a1174b73943667d983f9fd817
xiaotong <83378122@163.com>
2025-01-29 22:16:18 +0800

    初始化

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Logo from "../moralis-logo.svg";
+import Eth from "../eth.svg";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="logo" className="logo" />
+        <div className="headerItem">Swap</div>
+        <div className="headerItem">Tokens</div>
+      </div>
+      <div className="rightH">
+        <img src={Eth} alt="eth" className="eth" />
+        Ethereum
+      </div>
+      <div className="connectButton">Connect</div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
